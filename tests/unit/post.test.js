48c4d34ae988002091724817dc69975211ce6bd4
AryanK1511 <aryankhurana1511@gmail.com>
2024-06-03 08:07:37 -0400

    Add unit tests for fragments post route

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -8,16 +8,14 @@ describe('POST /v1/fragments', () => {
     request(app).post('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
   test('authenticated users can create a fragment successfully', async () => {
-    // Check to see whether the user is authenticated
     const res = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
       .set('Content-Type', 'text/plain')
       .send('This is a fragment');
 
-    expect(res.status).toBe(201); // Adjust the status code according to your API's response
+    expect(res.status).toBe(201);
 
-    // Check the response structure
     expect(res.body).toHaveProperty('status');
     expect(res.body).toHaveProperty('fragment');
     expect(res.body.fragment).toHaveProperty('id');
@@ -26,5 +24,41 @@ describe('POST /v1/fragments', () => {
     expect(res.body.fragment).toHaveProperty('updated');
     expect(res.body.fragment).toHaveProperty('type');
     expect(res.body.fragment).toHaveProperty('size');
+  });
+
+  test('Should get a 400 status code from the server if a Content-Type that is not supported by the API is passed', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'application/json')
+      .send('This is a fragment');
+
+    expect(res.status).toBe(400);
+
+    expect(res.body).toEqual({
+      status: 'error',
+      error: {
+        code: 400,
+        message: 'Invalid Content Type Header',
+      },
+    });
+  });
+
+  test('Should get a 500 status code from the server if an invalid Content-Type is passed', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'numbers')
+      .send('This is a fragment');
+
+    expect(res.status).toBe(500);
+
+    expect(res.body).toEqual({
+      status: 'error',
+      error: {
+        code: 500,
+        message: 'invalid media type',
+      },
+    });
   });
 });
