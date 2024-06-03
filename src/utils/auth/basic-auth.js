b717f3eda988c00f1ae6b1968077c1989724ab9d
AryanK1511 <aryankhurana1511@gmail.com>
2024-06-03 14:17:10 -0400

    Add auth middleware

--- src/utils/auth/basic-auth.js
+++ src/utils/auth/basic-auth.js
@@ -1,7 +1,8 @@
 const auth = require('http-auth');
-const passport = require('passport');
 const authPassport = require('http-auth-passport');
 const logger = require('../../logger');
+
+const authorize = require('./auth-middleware');
 
 // We expect HTPASSWD_FILE to be defined.
 if (!process.env.HTPASSWD_FILE) {
@@ -19,4 +20,4 @@ module.exports.strategy = () =>
     })
   );
 
-module.exports.authenticate = () => passport.authenticate('http', { session: false });
+module.exports.authenticate = () => authorize('http');
