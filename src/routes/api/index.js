b3e8605abb0b278a5999a6d7e6264448dfa496af
AryanK1511 <aryankhurana1511@gmail.com>
2024-06-03 07:19:13 -0400

    Add post fragments route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const { Fragment } = require('../../model/fragment');
+const contentType = require('content-type');
+
+// Middleware to parse binary blobs as body
+const rawBody = () =>
+  express.raw({
+    inflate: true,
+    limit: '5mb',
+    type: (req) => {
+      // See if we can parse this content type. If we can, `req.body` will be
+      // a Buffer (e.g., `Buffer.isBuffer(req.body) === true`). If not, `req.body`
+      // will be equal to an empty Object `{}` and `Buffer.isBuffer(req.body) === false`
+      const { type } = contentType.parse(req);
+      return Fragment.isSupportedType(type);
+    },
+  });
 
 // Create a router on which to mount our API endpoints
 const router = express.Router();
 
 // GET /v1/fragments
 router.get('/fragments', require('./get'));
+
+// POST /v1/fragments
+router.post('/fragments', rawBody(), require('./post'));
 
 module.exports = router;
---
+++ src/routes/api/post.js
@@ -0,0 +1,35 @@
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+const { Fragment } = require('../../model/fragment');
+const contentType = require('content-type');
+const logger = require('../../logger');
+const hashEmail = require('../../hash');
+
+// Create a fragment for the user
+module.exports = async (req, res) => {
+  // Check if the body type is a Buffer
+  if (Buffer.isBuffer(req.body)) {
+    // Hash the user's email
+    const hashedUserEmail = hashEmail(req.user);
+
+    // Get the content type
+    const { type } = contentType.parse(req);
+
+    // Create a new fragment
+    let fragment = new Fragment({
+      ownerId: hashedUserEmail,
+      type: type,
+      size: req.body.length,
+    });
+
+    // Save the fragment and the data of the fragment
+    await fragment.save();
+    await fragment.save(req.body);
+
+    // Fetch the fragment once it is saved
+    const result = await Fragment.byId(hashedUserEmail, fragment.id);
+
+    res.status(201).send(createSuccessResponse({ fragment: result }));
+  } else {
+    res.status(400).send({ error: 'Invalid content type' });
+  }
+};
