75ff81b944fdc5a8360c4ef68fd89d3497710371
AryanK1511 <aryankhurana1511@gmail.com>
2024-07-12 13:34:42 -0400

    Made post route more robust

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -2,29 +2,32 @@ const { createSuccessResponse, createErrorResponse } = require('../../response')
 const { Fragment } = require('../../model/fragment');
 const contentType = require('content-type');
 const logger = require('../../logger');
-const { validateFragment } = require('../../utils/tools/validateFragment');
+const { validateFragment } = require('../../utils/types/typeValidation');
 
 // ===== Create a fragment for the user =====
 module.exports.createFragment = async (req, res) => {
   try {
     logger.info('Creating a fragment for the user');
 
-    // Throw an error if the body is not a Buffer and\or is not supported by our application
+    // Throw an error if the body is not a Buffer and/or is not supported by our application
     if (!Buffer.isBuffer(req.body)) {
       logger.error('Unsupported Content-Type Header');
       return res.status(415).send(createErrorResponse(415, 'Unsupported Content-Type'));
     }
 
+    // The body of the fragment should not be empty
     if (!(req.body.length > 0)) {
       logger.error('Fragment cannot be null');
       return res.status(400).send(createErrorResponse(400, 'Fragment cannot be null'));
     }
 
     // Parse the content type from the request
-    const { type } = contentType.parse(req);
+    const parsedContentType = contentType.parse(req);
+    const { type, parameters } = parsedContentType;
+    const charset = parameters.charset || null; // Default to null if there are no parameters specified
     const size = req.body.length;
 
-    logger.debug({ type, size }, 'Parsed content type and size from request');
+    logger.debug({ type, charset, size }, 'Parsed content type, charset, and size from request');
 
     // Check whether the fragment data matches the content type
     // If the fragment is invalid, the function throws an error
@@ -39,7 +42,7 @@ module.exports.createFragment = async (req, res) => {
     // Create and save the new fragment
     const fragment = new Fragment({
       ownerId: req.user, // Email is hashed already due to the middleware
-      type: type,
+      type: charset ? `${type}; charset=${charset}` : type, // Only add the charset if it is not null
       size: size,
     });
 
