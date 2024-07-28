4d053bc41b8f4c70a3b1223a5ba8c3c2dfe93b78
AryanK1511 <aryankhurana1511@gmail.com>
2024-07-28 10:35:01 -0400

    Add env file config

--- src/utils/auth/index.js
+++ src/utils/auth/index.js
@@ -1,10 +1,14 @@
 // Use Amazon Cognito for Authentication in production
-if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
+if (
+  process.env.USE_AWS_AUTH === 'true' &&
+  process.env.AWS_COGNITO_POOL_ID &&
+  process.env.AWS_COGNITO_CLIENT_ID
+) {
   module.exports = require('./cognito');
 }
 
 // Also allow for an .htpasswd file to be used in development and for running tests
-else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
+else if (process.env.USE_AWS_AUTH !== 'true' && process.NODE_ENV !== 'production') {
   module.exports = require('./basic-auth');
 }
 
